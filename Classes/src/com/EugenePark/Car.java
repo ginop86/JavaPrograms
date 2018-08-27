b9f4866eac9681b03870c6781ca1f3d5ef196d2f
Eugene park <eugenepark@Eugenes-MacBook-Pro.local>
2018-08-27 15:39:46 -0700

    another commit

--- Classes/src/com/EugenePark/Car.java
+++ Classes/src/com/EugenePark/Car.java
@@ -2,5 +2,24 @@ package com.EugenePark;
 
 public class Car {
 
+    //characteristics of car
+    // the key word private means: don't allow these variables to be accessible outside of this Car class.
+    private int doors;
+    private int wheels;
+    private String model;
+    private String engine;
+    private String color;
+
+    //the whole point in setting methods in different classes is to not overwrite them in the Main Class.
+
+    //a public method
+    //this method updates the model variable. It SETS the model variable to whatever we want.
+    public void setModel(String model){
+        this.model = model;
+    }
+    // Notice the key work 'void' is not being used. Because this method is returning something. This method GETs the model variable.
+    public String getModel() {
+        return this.model;
+    }
 
 }
--- Classes/src/com/EugenePark/Main.java
+++ Classes/src/com/EugenePark/Main.java
@@ -3,6 +3,14 @@ package com.EugenePark;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        /*
+        using the 'new' key word initialize the new object
+         */
+        Car bmw = new Car();
+        Car honda = new Car();
+
+        bmw.setModel("7 Series");
+        System.out.println("Model is " + bmw.getModel());
+
     }
 }
